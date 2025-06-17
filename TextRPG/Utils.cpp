ef9bcb1fbca9b93f49500f0fc6954aea0b58137d
Gcrin <94522022+Gcrin@users.noreply.github.com>
2025-06-17 14:43:37 +0900

    유틸리티 기능 추가: 난수 생성을 위한 `Utils` 구현

---
+++ TextRPG/Utils.cpp
@@ -0,0 +1,10 @@
+#include "Utils.h"
+#include <random>
+
+int Utils::getRandomInt(int min, int max)
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(min, max);
+    return dist(gen);
+}
