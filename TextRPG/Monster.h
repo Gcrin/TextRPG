3af6b36144ecaff399e2848dd334bc436c881cae
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-23 10:23:34 +0900

    Add Monster

--- TextRPG/Monster.h
+++ TextRPG/Monster.h
@@ -1,7 +1,19 @@
 #pragma once
+#include <string>
+#include <memory>
+
+class Item;
 
 class Monster
 {
 public:
-    
+    virtual ~Monster() = default;
+
+    virtual std::string GetName() const = 0;
+    virtual int GetHealth() const = 0;
+    virtual int GetAttack() const = 0;
+    virtual void TakeDamage(int damage) = 0;
+    virtual std::unique_ptr<Item> DropItem() = 0;
+    virtual int GetGold()  = 0;
+    virtual int GetExperience() const = 0;
 };
