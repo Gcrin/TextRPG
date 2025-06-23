305d44a618dc8081d68a71ce277a828b9443bebc
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-23 10:23:24 +0900

    Add Item

--- TextRPG/Item.h
+++ TextRPG/Item.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <string>
+#include <memory>
+
+class Character;
 
 class Item
 {
 public:
-    
+    virtual ~Item() = default;
+    virtual std::string GetName() = 0;
+    virtual int GetPrice() = 0;
+    virtual void Use(Character* Character) = 0;
+    virtual std::unique_ptr<Item> Clone() const = 0;
 };
