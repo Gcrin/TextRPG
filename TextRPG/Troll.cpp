7230480fd0212b791ecd7babe63eacc34eda283f
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 11:51:36 +0900

    fix Troll
    상위 클래스 Monster 수정에 의한 Troll 코드 수정

--- TextRPG/Troll.cpp
+++ TextRPG/Troll.cpp
@@ -1,14 +1,39 @@
 #include "Troll.h"
 #include "Item.h"
+#include "AttackBoost.h"
+#include "HealthPotion.h"
+#include "Util.h"
+#include <iostream>
 
-Troll::Troll(int level) : Name("Goblin"), Health(level * 20), Attack(level * 10) {}
-
-void Troll::TakeDamage(int damage)
+Troll::Troll(int level)
+	: Monster("Troll", level * 25, level * 10, 100)
 {
-	Health -= damage;
+
 }
 
-Item* Troll::DropItem()
+int Troll::GetGold()
 {
+	return RandomUtil::GetRandomInt(10, 20);
+}
+
+std::unique_ptr<Item> Troll::DropItem()
+{
+
+	int roll = RandomUtil::GetRandomInt(1, 100);
+	if (roll <= 20)
+	{
+		int itemTypeDist = RandomUtil::GetRandomInt(0, 1);
+		if (itemTypeDist == 0)
+		{
+			std::cout << GetName() << "이 HealthPotion을 드랍.\n";
+			return std::make_unique<HealthPotion>();
+		}
+		else if (itemTypeDist == 1)
+		{
+			std::cout << GetName() << "이 AttackBoost를 드랍.\n";
+			return std::make_unique<AttackBoost>();
+		}
+
+	}
 	return nullptr;
 }
--- TextRPG/Troll.h
+++ TextRPG/Troll.h
@@ -3,20 +3,10 @@
 
 class Troll : public Monster
 {
-private:
-	std::string Name;
-	int Health;
-	int Attack;
-
 public:
 	Troll(int level);
 	~Troll() override = default;
 
-	std::string GetName() override { return Name; }
-	int GetHealth() override { return Health; }
-	int GetAttack() override { return Attack; }
-
-	void TakeDamage(int damage) override;
-	Item* DropItem() override;
-
+	std::unique_ptr<Item> DropItem() override;
+	int GetGold()  override;
 };
