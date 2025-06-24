d3c7dc4ab0203b991398b1daf0f23b4251f0c7f4
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 11:57:33 +0900

    fix Item
    1. protected 멤버 변수 생성
    2. Item 생성자 추가
    2. Item에서 구현이 설계상 더 용이한 가상함수는 함수 바디 작성

--- TextRPG/Item.cpp
+++ TextRPG/Item.cpp
@@ -1,1 +1,7 @@
 #include "Item.h"
+
+Item::Item(ItemID id, std::string name, int price)
+	: ID(id), Name(name), Price(price)
+{
+
+}
--- TextRPG/Item.h
+++ TextRPG/Item.h
@@ -1,15 +1,27 @@
 #pragma once
+#include "Enums.h"
 #include <string>
 #include <memory>
+
+
 
 class Character;
 
 class Item
 {
+protected:
+    std::string Name;
+    int Price;
+    ItemID ID;
+
 public:
+    Item(ItemID id, std::string name, int price);
     virtual ~Item() = default;
-    virtual std::string GetName() = 0;
-    virtual int GetPrice() = 0;
+
     virtual void Use(Character* Character) = 0;
     virtual std::unique_ptr<Item> Clone() const = 0;
+
+    virtual ItemID GetId() const { return ID; }
+    virtual std::string GetName() const { return Name; }
+    virtual int GetPrice() const { return Price; }
 };
