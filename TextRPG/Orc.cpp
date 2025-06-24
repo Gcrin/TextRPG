f824347b764683356dd54cd08ba79609eaf338df
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 11:51:05 +0900

    fix Orc
    상위 클래스 Monster의 수정에 의한 Orc코드 수정

--- TextRPG/Orc.cpp
+++ TextRPG/Orc.cpp
@@ -1,14 +1,40 @@
 #include "Orc.h"
 #include "Item.h"
+#include "AttackBoost.h"
+#include "HealthPotion.h"
+#include "Util.h"
+#include <iostream>
 
-Orc::Orc(int level) : Name("Goblin"), Health(level * 30), Attack(level * 5) {}
-
-void Orc::TakeDamage(int damage)
+Orc::Orc(int level)
+	: Monster("Orc", level * 30, level * 5, 80)
 {
-	Health -= damage;
+
 }
 
-Item* Orc::DropItem()
+int Orc::GetGold()
 {
+	return RandomUtil::GetRandomInt(10, 20);
+}
+
+std::unique_ptr<Item> Orc::DropItem()
+{
+
+	int roll = RandomUtil::GetRandomInt(1, 100);
+	if (roll <= 10)
+	{
+		int itemTypeDist = RandomUtil::GetRandomInt(0, 1);
+		if (itemTypeDist == 0)
+		{
+			std::cout << GetName() << "이 HealthPotion을 드랍.\n";
+			return std::make_unique<HealthPotion>();
+		}
+		else if (itemTypeDist == 1)
+		{
+			std::cout << GetName() << "이 AttackBoost를 드랍.\n";
+			return std::make_unique<AttackBoost>();
+		}
+
+	}
 	return nullptr;
 }
+
--- TextRPG/Orc.h
+++ TextRPG/Orc.h
@@ -3,20 +3,11 @@
 
 class Orc : public Monster
 {
-private:
-	std::string Name;
-	int Health;
-	int Attack;
-
 public:
 	Orc(int level);
 	~Orc() override = default;
 
-	std::string GetName() override { return Name; }
-	int GetHealth() override { return Health; }
-	int GetAttack() override { return Attack; }
-
-	void TakeDamage(int damage) override;
-	Item* DropItem() override;
+	std::unique_ptr<Item> DropItem() override;
+	int GetGold()  override;
 
 };
