4844aff48071136e969971551b3b1d28c7e935dc
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 11:49:41 +0900

    update Monster
    Monster.h 변경에 의한 Monster.cpp 함수 구현 추가

--- TextRPG/Monster.cpp
+++ TextRPG/Monster.cpp
@@ -1,1 +1,14 @@
 #include "Monster.h"
+#include <algorithm>
+
+Monster::Monster(std::string name, int health, int attack, int experience)
+	:Name(std::move(name)), Health(health), Attack(attack), Experience(experience)
+{
+
+}
+
+void Monster::TakeDamage(int damage)
+{
+	Health -= damage;
+	Health = std::max(Health, 0);
+}
