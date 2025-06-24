1e003fd4bbc4b2a3cfe825e2f2078f59e6cb1b5a
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 12:02:58 +0900

    update main
    게임 실행 추가

--- TextRPG/Shop.h
+++ TextRPG/Shop.h
@@ -14,7 +14,14 @@ public:
     Shop();
     ~Shop();
 
+    
     void DisplayItems() const;
+    /**
+     * 상점에서 플레이어가 특정 아이템을 구매하는 메서드입니다.
+     *
+     * @param index 구매하려는 아이템의 인덱스입니다. 아이템 리스트의 유효한 범위 내에 있어야 합니다.
+     * @param player 아이템을 구매하려는 플레이어 캐릭터의 포인터입니다.
+     */
     void BuyItem(int index, Character* player);
     void SellItem(int index, Character* player);
     void AddItem(std::unique_ptr<Item> item);
