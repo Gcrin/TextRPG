3f11539b1297848e54a56290b3b9ece673073d79
BaekSeungSu <91333092+BaekSeungSu@users.noreply.github.com>
2025-06-24 11:50:50 +0900

    fix Goblin
    상위 클래스 Monster의 수정에 의한 Golbin 코드 수정

--- TextRPG/Goblin.cpp
+++ TextRPG/Goblin.cpp
@@ -2,42 +2,35 @@
 #include "Item.h"
 #include "AttackBoost.h"
 #include "HealthPotion.h"
-#include "iostream"
-#include <algorithm>
+#include "Util.h"
+#include <iostream>
 
-Goblin::Goblin(int level, std::mt19937& engine)
-	: Name("Goblin"), Health(level * 20), Attack(level * 5), Experience(50), randomEngine(engine)
+Goblin::Goblin(int level)
+	: Monster("Goblin", level * 20, level * 5, 1000)
 {
 	
 }
 
 int Goblin::GetGold()
 {
-	std::uniform_int_distribution<int> goldDist(10, 20);
-	return goldDist(randomEngine);
-}
-
-void Goblin::TakeDamage(int damage)
-{
-	Health -= damage;
-	Health = std::max(Health, 0);
+	return RandomUtil::GetRandomInt(10, 20);
 }
 
 std::unique_ptr<Item> Goblin::DropItem()
 {
-	std::uniform_int_distribution<int> dropDist(1, 100);
-	int roll = dropDist(randomEngine);
-	if (roll<=10)
+	
+	int roll = RandomUtil::GetRandomInt(1, 100);
+	if (roll<=5)
 	{
-		std::uniform_int_distribution<int> itemTypeDist(0, 1);
-		if (itemTypeDist(randomEngine) == 0)
+		int itemTypeDist = RandomUtil::GetRandomInt(0, 1);
+		if (itemTypeDist == 0)
 		{
-			std::cout << GetName() <<"이 HealthPotion을 떨어뜨렸다.\n";
+			std::cout << GetName() <<"이 HealthPotion을 드랍.\n";
 			return std::make_unique<HealthPotion>();
 		}
-		else
+		else if(itemTypeDist == 1)
 		{
-			std::cout << GetName() <<"이 AttackBoost를 떨어뜨렸다.\n";
+			std::cout << GetName() <<"이 AttackBoost를 드랍.\n";
 			return std::make_unique<AttackBoost>();
 		}
 		
--- TextRPG/Goblin.h
+++ TextRPG/Goblin.h
@@ -1,28 +1,12 @@
 #pragma once
 #include "Monster.h"
-#include <random>
 
 class Goblin : public Monster 
 {
-private:
-	std::string Name;
-	int Health;
-	int Attack;
-	int Experience;
-
-	std::mt19937& randomEngine;
-	
 public:
-	Goblin(int level, std::mt19937& engine);
+	Goblin(int level);
 	~Goblin() override = default;
-
-	std::string GetName() const override { return Name; }
-	int GetHealth() const override { return Health; }
-	int GetAttack() const override { return Attack; }
-	int GetExperience() const override { return Experience; }
-	int GetGold()  override;
 	
-	void TakeDamage(int damage) override;
 	std::unique_ptr<Item> DropItem() override;
-
+	int GetGold()  override;
 };
